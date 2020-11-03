91f351a6dd7c246f5624b07f7a16f091c50c2aeb
Durand <guillemettedlv@gmail.com>
2020-11-03 18:14:16 +0100

    test

---
+++ test_eventbrite-app-thp/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root to: 'events#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events
+  resources :users
+end
---
+++ test_eventbrite-app-thp/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Event.destroy_all
+
+10.times do 
+  @user = User.create!(email: Faker::Name.first_name+"@yopmail.com",password: "random",description: Faker::Lorem.sentence,first_name: Faker::Name.first_name,last_name: Faker::Name.last_name)
+ @event = Event.create!(title: Faker::Lorem.sentence(3),start_date: Faker::Time.forward(90, :day),duration: rand(1..40)*5,description: Faker::Lorem.paragraph,price: rand(1..100),location: Faker::Address.city,organizer: @user)
+end
